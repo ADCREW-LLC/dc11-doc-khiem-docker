5eb84ceecea992c89fac0069b1549a4b5c490e7a
Ubuntu <azureuser@Ubuntu.1mvfjrhk0mgencqhqtqpv1v3yh.cbrx.internal.cloudapp.net>
2023-10-06 02:17:25 +0000

    Fix requirements.txt

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-git stash
-git pull
 # Remoce items and retest
 docker rm `docker ps -a --format 'json' | grep -Po '"[0-9a-z]{12}"' | grep -Po '[0-9a-z]*'`
 docker rmi `docker images --format 'json' | grep -Po '"[0-9a-z]{12}"' | grep -Po '[0-9a-z]*'`
